7e943e432bf3dec32b6de0f1798f391dd7574e05
evanastevska <enastevska@ufl.edu>
2024-10-27 20:44:33 -0400

    Add backend functionality for signup and login page

--- snackhack-backend/server.js
+++ snackhack-backend/server.js
@@ -1,16 +1,51 @@
-const express = require('express');
+const express = require("express");
+const mysql = require('mysql');
 const cors = require('cors');
+
+
+//code learned from https://youtu.be/F53MPHqOmYI?si=XuTNyzR7o-POqHBF
+
+
 const app = express();
-
-// Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
 
-// SnackHack
-app.get('/api/SnackHack', (req, res) => {
-    res.send('Welcome to SnackHack!');
-});
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "signup"
+})
 
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`SnackHack server is running on port ${PORT}`));
+app.post('/signup', (req, res)=> {
+    const sql = "INSERT INTO login (`name`, `email`, `password`) VALUES (?)"
+    const values = [
+        req.body.name,
+        req.body.email,
+        req.body.password
+    ]
+    db.query(sql, [values], (err, data)=> {
+        if(err){
+            return res.json("Error");
+        }
+        return res.json(data);
+    })
+})
+
+app.post('/login', (req, res)=> {
+    const sql = "SELECT * FROM login WHERE `email` = ? AND `password` = ?"
+    db.query(sql, [req.body.email, req.body.password], (err, data)=> {
+        if(err){
+            return res.json("Error");
+        }
+        if(data.length > 0){
+            return res.json("Success");
+        } else {
+            return res.json("Fail");
+        }
+    })
+})
+
+app.listen(8081, ()=> {
+    console.log("listening")
+})
